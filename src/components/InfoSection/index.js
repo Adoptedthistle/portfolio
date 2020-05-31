6e4879e900c6db1f58468bf66c89768dfd5e15ac
Adopted Thistle <sbarkle.1@googlemail.com>
2020-05-31 22:55:11 +0100

    tweaks to content

--- src/components/InfoSection/index.js
+++ src/components/InfoSection/index.js
@@ -8,17 +8,26 @@ function InfoSection(){
     <h2>About Me</h2>
     <p>A former marketer and outreach worker embarking on a new adventure into the world of tech.</p>
     <p></p>
-    <h2>Previous xperience</h2>
-    <p>As a qualified I have developed a broad range of transferable skills including:</p>
+    
+    <h2>Previous work experience</h2>
+    <p>As a qualified marketer I have developed a broad range of transferable skills including:</p>
+    <ul>
+        <li>Communication</li>
+        <li>Project Management</li>
+        <li>Teamwork</li>
+        <li>Problem Solving</li>
+        <li>Line Management</li>
+    </ul>
 
 
-    <p>Industries I have worked include:</p>
+    <p>Industries I have previously worked in include:</p>
     <ul>
     <li>Higher Education, specifically Arts, Design, Humanities, Social Sciences and Technology</li>
     <li>Book Publishing</li>
     <li>Design (Agency)</li>
     </ul>
     <h2>Technical Skills</h2>
+
 </div>
     );
 }
